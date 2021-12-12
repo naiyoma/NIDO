aedee93f8a0886f0c4d79cf01509a1044fb9634b
Naiyoma <lankas.aurelia@gmail.com>
2021-12-13 00:03:14 +0300

    create a selction for computer choice

--- game.js
+++ game.js
@@ -2,4 +2,7 @@ let choice = ["rock", "paper", "scissors"];
 let random = Math.floor(Math.random() * 3);
 let computer = choice[random];
 
-console.log(computer)
+let computerSelection = function computerPlay() {
+    return computer;
+}
+console.log(computerSelection)
